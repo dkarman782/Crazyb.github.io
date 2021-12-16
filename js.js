991e064d9f35364ba191c9be04e71aada0026528
fullunlocked <96154143+fullunlocked@users.noreply.github.com>
2021-12-16 09:54:47 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,14 +1,7 @@
 function time_count(){
-	var date = new Date();
-	var now = "";
-	now = date.getFullYear()+"年";
-	now = now + (date.getMonth()+1)+"月";
-	now = now + date.getDate()+"日";
-	now = now + date.getHours()+"时";
-	now = now + date.getMinutes()+"分";
-	now = now + date.getSeconds()+"秒";
+	var now = new Date().getTime();
 	var input_time = document.getElementById("input_time").value;
-	document.getElementById("time_remain").innerHTML = date;
+	document.getElementById("time_remain").innerHTML = now;
 }
 
 function war_count(){
